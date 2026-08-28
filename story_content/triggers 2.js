debcac781ebc17a84c20d45774ed54e5eda911cb
BLMichaels <155132733+BLMichaels@users.noreply.github.com>
2026-08-28 09:19:53 -0400

    SimBox_Pediatric_Cardiac_Codes
    SimBox_Pediatric_Cardiac_Codes

---
+++ story_content/triggers 2.js
@@ -0,0 +1,128 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6qTzvqpDQSR":
+        Script1();
+        break;
+      case "5eMa6w9ze7F":
+        Script2();
+        break;
+      case "6eO20CTjhEO":
+        Script3();
+        break;
+      case "5VwluABpb7T":
+        Script4();
+        break;
+      case "6cK2Abcqr0w":
+        Script5();
+        break;
+      case "5wEbYIeV68h":
+        Script6();
+        break;
+      case "5W0XcdqLaaM":
+        Script7();
+        break;
+      case "6IY22t8vprw":
+        Script8();
+        break;
+      case "6UVBLduk38R":
+        Script9();
+        break;
+      case "6aQJfXfCIcS":
+        Script10();
+        break;
+      case "5pP0F8LOFHm":
+        Script11();
+        break;
+      case "6lj7AtbHEHg":
+        Script12();
+        break;
+      case "6RCvzH5xK0b":
+        Script13();
+        break;
+      case "5kg7U8RnVhK":
+        Script14();
+        break;
+      case "6hBxegvJoIZ":
+        Script15();
+        break;
+      case "6VgUrcBhv1O":
+        Script16();
+        break;
+      case "6pNOVuItMw6":
+        Script17();
+        break;
+      case "61eXlndY90w":
+        Script18();
+        break;
+      case "67xGxLZ3p5P":
+        Script19();
+        break;
+      case "6E0vXIDtN2f":
+        Script20();
+        break;
+      case "5ibvB8S5rhl":
+        Script21();
+        break;
+      case "6RVtBPXAjZ4":
+        Script22();
+        break;
+      case "6FeS9QTHDgG":
+        Script23();
+        break;
+      case "5tXuHBvkZNc":
+        Script24();
+        break;
+      case "6p5gPP1HkyD":
+        Script25();
+        break;
+      case "6eUnGbKViEe":
+        Script26();
+        break;
+      case "6OPW8hs3jAb":
+        Script27();
+        break;
+      case "6OyXHRfWtIq":
+        Script28();
+        break;
+      case "68uBpJapLR1":
+        Script29();
+        break;
+      case "6P7ba35FcW0":
+        Script30();
+        break;
+      case "67HX4toFKzW":
+        Script31();
+        break;
+      case "6XQViwmosfl":
+        Script32();
+        break;
+      case "6fyWoDuLA2i":
+        Script33();
+        break;
+      case "6AtrHgUK2YN":
+        Script34();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
